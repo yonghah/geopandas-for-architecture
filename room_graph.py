4ee6ef2a9a2f86d6cba5f6c16a3ffd1a5341f1b5
Yongha Hwang <yongha.hwang@gmail.com>
2016-12-16 11:31:58 -0500

    add notebook

--- room_graph.py
+++ room_graph.py
@@ -1,4 +1,6 @@
 import geopandas as gpd
+import networkx as nx
+import pandas as pd
 import matplotlib.pyplot as plt
 import os
 
@@ -11,14 +13,23 @@ print(rooms.head())
 fig, ax = plt.subplots()
 fig.set_size_inches(12, 12)
 ax.axison = False
-rooms.plot(ax=ax, linewidth=0.3, column='Shape_Area', cmap='viridis')
-
-# create room's centroids
-rooms.geometry.centroid.plot(ax=ax, markersize=8, color='k')
+rooms.plot(ax=ax, linewidth=0.3, color='#DDDDDD')
+reference = gpd.read_file(gdb_path, layer=3)
+reference.plot(ax=ax, linewidth=0.7, color='k')
 ax.set_aspect('equal')
 plt.show()
 
 
+# create room's centroids
+fig, ax = plt.subplots()
+fig.set_size_inches(12, 12)
+ax.axison = False
+rooms.plot(ax=ax, linewidth=0.3, color='#DDDDDD')
+rooms.geometry.centroid.plot(ax=ax, markersize=6, color='#6677AA')
+ax.set_aspect('equal')
+plt.show()
+
+# create edges if two room polygons touch each other
 from shapely.geometry import LineString
 lines = {'geometry':[], 'from':[], 'to':[]}
 buffer_size = 0.02
@@ -26,20 +37,33 @@ for index1, room1 in rooms.iterrows():
     for index2, room2 in rooms.iterrows():
         if room1.geometry.buffer(buffer_size).intersects(room2.geometry.buffer(buffer_size)) \
             and index1 > index2:
-            lines["from"].append(room1)
-            lines["to"].append(room2)
+            lines["from"].append(index1)
+            lines["to"].append(index2)
             lines["geometry"].append(LineString([room1.geometry.centroid, room2.geometry.centroid]))
 
 edges = gpd.GeoDataFrame(lines)
 
+
+
+# let's create network, not just displaying..
+room_network = nx.Graph()
+for i, edge in edges.iterrows():
+    room_network.add_edge(edge["from"], edge["to"])
+
+# calculate closeness and add the values to rooms
+closeness = nx.closeness_centrality(room_network)
+rooms['closeness'] = pd.Series(closeness)
+
+# color rooms by closeness
 fig, ax = plt.subplots()
 fig.set_size_inches(12, 12)
 ax.axison = False
-rooms.plot(ax=ax, linewidth=0.3, column='Shape_Area', cmap='viridis')
-edges.plot(ax=ax, linewidth=0.7, color='r')
-rooms.geometry.centroid.plot(ax=ax, markersize=8, color='k')
+rooms.plot(ax=ax, linewidth=0.3, column='closeness', cmap='coolwarm')
+edges.plot(ax=ax, linewidth=0.3, color='#888888')
+rooms.geometry.centroid.plot(ax=ax, markersize=6, color='#DDDDDD')
 ax.set_aspect('equal')
 plt.show()
 
-# let's create network, not just displaying..
-import networkx as nx
+
+comm = nx.girvan_newman(room_network)
+print(comm)
